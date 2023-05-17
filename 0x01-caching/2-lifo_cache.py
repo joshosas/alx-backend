0c7c056fd8c3ba0cd939caed6ea697730694329e
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2023-05-17 21:50:40 +0000

    task 2

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""First-In First-Out caching module.
+"""
+from collections import OrderedDict
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """a class that allows storing, retreiving
+       and deleting of information using the 
+       Firet-In-First-Out (FIFO) Replacement Policy.
+    """
+
+    def __init__(self):
+        """Initializes the cache.
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Stores and delete an item in the cache (if Cache is full).
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(False)
+            print("DISCARD:", first_key)
+
+    def get(self, key):
+        """Retrieves an item by key.
+        """
+        if key is None:
+            return None
+        return self.cache_data.get(key)
